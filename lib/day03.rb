9fbceff3e5b6872ee56b80eceda8dcaf09801c5a
Chris Sinjakli <chris@sinjakli.co.uk>
2022-12-03 12:12:26 +0000

    Day 03 part b

--- lib/day03.rb
+++ lib/day03.rb
@@ -21,5 +21,22 @@ class Day03
   end
 
   def part_b(input)
+    input.
+      lines.
+      map(&:chomp).
+      map(&:chars).
+      map(&:to_set).
+      each_slice(3).
+      map { |group| group[0].intersection(group[1]).intersection(group[2]) }.
+      map(&:first).
+      map { |common_item|
+        case common_item
+        when /[A-Z]/
+          common_item.ord - 38
+        when /[a-z]/
+          common_item.ord - 96
+        end
+      }.
+      sum
   end
 end
