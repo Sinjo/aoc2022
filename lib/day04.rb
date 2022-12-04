dfc0380a02eb555826291b10c1388b693241b5fb
Chris Sinjakli <chris@sinjakli.co.uk>
2022-12-04 13:27:43 +0000

    Day 4

---
+++ lib/day04.rb
@@ -0,0 +1,45 @@
+require "set"
+
+class Day04
+  PAIR_REGEX = /(?<elf_one_start>\d+)-(?<elf_one_end>\d+),(?<elf_two_start>\d+)-(?<elf_two_end>\d+)/
+
+  ElfPair = Struct.new(:elf_one_start, :elf_one_end, :elf_two_start, :elf_two_end)
+
+  def part_a(input)
+    input.
+      lines.
+      map(&:chomp).
+      map { |line| PAIR_REGEX.match(line) }.
+      map { |line|
+        ElfPair.new(line[:elf_one_start],
+                    line[:elf_one_end],
+                    line[:elf_two_start],
+                    line[:elf_two_end])
+      }.map { |line|
+        [(line.elf_one_start..line.elf_one_end).to_set, (line.elf_two_start..line.elf_two_end).to_set]
+      }.map { |line|
+        line.first <= line.last || line.last <= line.first
+      }.filter { |line|
+        line
+      }.size
+  end
+
+  def part_b(input)
+    input.
+      lines.
+      map(&:chomp).
+      map { |line| PAIR_REGEX.match(line) }.
+      map { |line|
+        ElfPair.new(line[:elf_one_start],
+                    line[:elf_one_end],
+                    line[:elf_two_start],
+                    line[:elf_two_end])
+      }.map { |line|
+        [(line.elf_one_start..line.elf_one_end).to_set, (line.elf_two_start..line.elf_two_end).to_set]
+      }.map { |line|
+        (line.first & line.last).size > 0
+      }.filter { |line|
+        line
+      }.size
+  end
+end
