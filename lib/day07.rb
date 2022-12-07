91df83f55f71bc99f1ff1754ee05394b84ebeb9d
Chris Sinjakli <chris@sinjakli.co.uk>
2022-12-07 23:59:18 +0000

    Day 7 part b

--- lib/day07.rb
+++ lib/day07.rb
@@ -3,6 +3,50 @@ require "set"
 
 class Day07
   def part_a(input)
+    all_directories, file_sizes = parse(input)
+
+    Hash[all_directories.map { |dir|
+      if dir.to_s == "/"
+        total_size = file_sizes.values.sum
+      else
+        total_size = file_sizes.filter { |file, _|
+          file.to_s.start_with?(dir.to_s + "/")
+        }.values.sum
+      end
+
+      [dir, total_size]
+    }].filter { |dir, size|
+      size <= 100_000
+    }.values.sum
+  end
+
+  def part_b(input)
+    all_directories, file_sizes = parse(input)
+
+    directory_sizes = Hash[all_directories.map { |dir|
+      # This wouldn't need to be a separate case if I deduped // later on
+      if dir.to_s == "/"
+        total_size = file_sizes.values.sum
+      else
+        total_size = file_sizes.filter { |file, _|
+          file.to_s.start_with?(dir.to_s + "/")
+        }.values.sum
+      end
+
+      [dir, total_size]
+    }]
+
+    available = 70_000_000 - directory_sizes[Pathname.new("/")]
+    needed_for_update = 30_000_000 - available
+
+    directory_sizes.reject { |_, size|
+      size < needed_for_update
+    }.values.min
+  end
+
+  private
+
+  def parse(input)
     current_path = Pathname.new("/")
     all_directories = Set.new
     file_sizes = {}
@@ -28,21 +72,9 @@ class Day07
           file_sizes[current_path.join(Regexp.last_match[2])] = Integer(Regexp.last_match[1])
         end
       end
-
     end
 
-    Hash[all_directories.map { |dir|
-      total_size = file_sizes.filter { |file, _|
-        file.to_s.start_with?(dir.to_s + "/")
-      }.values.sum
-
-      [dir, total_size]
-    }].filter { |dir, size|
-      size <= 100_000
-    }.values.sum
-  end
-
-  def part_b(input)
+    [all_directories, file_sizes]
   end
 end
 
