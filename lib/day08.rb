694d40351940f4b44a4e20244e3f22912e809ede
Chris Sinjakli <chris@sinjakli.co.uk>
2022-12-08 20:53:36 +0000

    Day 8 part b

--- lib/day08.rb
+++ lib/day08.rb
@@ -43,5 +43,53 @@ class Day08
   end
 
   def part_b(input)
+    visible_trees = Set.new()
+
+    grid = input.lines.map(&:chomp).map { |line|
+      line.chars.map { |height| Integer(height) }
+    }
+    width = grid.first.size
+    height = grid.size
+
+    scenic_scores = []
+
+    (0...height).each do |y|
+      (0...width).each do |x|
+        # Any tree on the edge has a scenic score of 0
+        if x == 0 || y == 0 || x == width - 1 || y == height - 1
+          next
+        end
+
+        left_score = right_score = up_score = down_score = 0
+        (x - 1).downto(0).each_with_index { |inspect_x, distance|
+          if grid[y][inspect_x] >= grid[y][x] || inspect_x == 0
+            left_score = distance + 1
+            break
+          end
+        }
+        ((x + 1)...width).each_with_index { |inspect_x, distance|
+          if grid[y][inspect_x] >= grid[y][x] || inspect_x == (width - 1)
+            right_score = distance + 1
+            break
+          end
+        }
+        (y - 1).downto(0).each_with_index { |inspect_y, distance|
+          if grid[inspect_y][x] >= grid[y][x] || inspect_y == 0
+            up_score = distance + 1
+            break
+          end
+        }
+        ((y + 1)...height).each_with_index { |inspect_y, distance|
+          if grid[inspect_y][x] >= grid[y][x] || inspect_y == (height - 1)
+            down_score = distance + 1
+            break
+          end
+        }
+
+        scenic_scores << (left_score * right_score * up_score * down_score)
+      end
+    end
+
+    scenic_scores.max
   end
 end
