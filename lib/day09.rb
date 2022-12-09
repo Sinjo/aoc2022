401e8a88ded380dd532fd56bc517dcb9c27fa14c
Chris Sinjakli <chris@sinjakli.co.uk>
2022-12-09 23:53:33 +0000

    Day 9 part b

--- lib/day09.rb
+++ lib/day09.rb
@@ -71,5 +71,66 @@ class Day09
   end
 
   def part_b(input)
+    tail_visited = Set.new()
+    tail_visited.add(Point.new(0,0))
+
+    moves = input.lines.map(&:chomp).map(&:split).map { |line|
+      Move.new(line.first, Integer(line.last))
+    }
+
+    positions = Array.new(10) { Point.new(0, 0) }
+
+    moves.each do |move|
+      move.distance.times do
+        head_position = positions.first
+
+        new_head_x = head_position.x + MOVE_TRANSLATIONS[move.direction].first
+        new_head_y = head_position.y + MOVE_TRANSLATIONS[move.direction].last
+        new_head_position = Point.new(new_head_x, new_head_y)
+
+        positions[0] = new_head_position
+
+        # positions.each_cons(2).with_index do |(head_position, tail_position), index|
+        (0...(positions.size - 1)).each do |index|
+          head_position = positions[index]
+          tail_position = positions[index + 1]
+
+
+          # The tail doesn't move if we're already within one square of the head (or in the same square)
+          if (head_position.x - tail_position.x).abs <= 1 && (head_position.y - tail_position.y).abs <= 1
+            positions[index + 1] = tail_position
+            next
+          end
+
+          # We're aligned on one of the two axes, so the move is simple
+          if head_position.x == tail_position.x 
+            new_tail_y = (head_position.y + tail_position.y) / 2
+            tail_position = Point.new(tail_position.x, new_tail_y)
+            positions[index + 1] = tail_position
+
+            next
+          elsif head_position.y == tail_position.y
+            new_tail_x = (head_position.x + tail_position.x) / 2
+            tail_position = Point.new(new_tail_x, tail_position.y)
+            positions[index + 1] = tail_position
+
+            next
+          end
+
+          # We need to move diagonally
+          # Construct a translation based on the relative positions of head and tail
+          translation_x = (head_position.x - tail_position.x) <=> 0
+          translation_y = (head_position.y - tail_position.y) <=> 0
+
+          tail_position = Point.new(tail_position.x + translation_x, tail_position.y + translation_y)
+
+          positions[index + 1] = tail_position
+        end
+
+        tail_visited.add(positions.last)
+      end
+    end
+
+    tail_visited.size
   end
 end
